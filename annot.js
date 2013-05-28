d4948b54aac269fb32b4ae65aa7f5c7050a8fe23
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-05-27 18:10:49 -0700

    shuffling directories

--- annot.js
+++ annot.js
@@ -1,16 +1,29 @@
-
 'use strict';
 
+
+// Define Editor
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/chaos");
-    
 var SrcMode = require("ace/mode/haskell").Mode;
 editor.getSession().setMode(new SrcMode());
 
-var fooBar = "gob";
+
+
+function updateAnnotValue($scope, newVal){
+  $scope.annotValue = newVal;
+}
 
 // Angular Controller
 function AnnotDemoCtrl($scope, $http, $location){
+  
   $scope.annotValue = "Zogbert Friggleby";
-  fooBar = "poop";
+
+  editor.on("click", function(ev){
+    $scope.$apply(function () {
+      var pos    = ev.getDocumentPosition();
+      var tok    = editor.session.getTokenAt(pos.row, pos.column);
+      var fooBar = "Row = " + pos.row + " Col " + pos.column + " token = " + tok.value ;
+      updateAnnotValue($scope, fooBar);
+    });
+  });
 }
